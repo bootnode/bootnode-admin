3a01cbaa538c92e782939fd7c5a27af2d0572811
David Tai <david@verus.io>
2019-05-07 17:27:24 -0700

    fix endpoint

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,2 @@
-export let BOOTNODE_ENDPOINT = 'http://192.168.1.20:4000/' //'https://bootnode-api.hanzo.ai/'
+export let BOOTNODE_ENDPOINT = 'https://bootnode-api.hanzo.io/'
 export let BOOTNODE_KEY = 'fLcLu7OLD81aR9jf'
