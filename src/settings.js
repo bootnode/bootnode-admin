69975fea7b21117ef121153dc918004f0951d1ea
David Tai <david@verus.io>
2019-05-24 16:02:54 -0500

    fix outdated prod path

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export const PRODUCTION   = typeof window === 'undefined' ? true : window.location.href.indexOf('bootnode') >= 0
 
-export let BOOTNODE_ENDPOINT = PRODUCTION ? 'https://bootnode-api.hanzo.ai/' : 'http://localhost:4000/'
+export let BOOTNODE_ENDPOINT = PRODUCTION ? 'https://bootnode-api.hanzo.io/' : 'http://localhost:4000/'
